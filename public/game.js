bfb7eebbfea84a61c8c074d1aa61fa1d4400fe78
spidercogaming-dotcom <spider.co.gaming@gmail.com>
2026-02-16 08:03:16 +0530

    Update game.js

--- public/game.js
+++ public/game.js
@@ -10,6 +10,8 @@ const MAP_HEIGHT = 2000;
 
 let players = {};
 let bullets = [];
+let coins = [];
+let healthPacks = [];
 let myId = null;
 let camera = { x: 0, y: 0 };
 
@@ -20,16 +22,20 @@ socket.on("connect", () => myId = socket.id);
 socket.on("state", (data) => {
     players = data.players;
     bullets = data.bullets;
+    coins = data.coins || [];
+    healthPacks = data.healthPacks || [];
 
     if (players[myId]) {
         document.getElementById("coins").innerText = players[myId].coins;
         document.getElementById("weapon").innerText = players[myId].weapon;
+        document.getElementById("hp").innerText = players[myId].hp;
     }
 });
 
 socket.on("crateResult", (weapon) => {
     const box = document.getElementById("crateAnimation");
     box.innerText = "You got: " + weapon + "!";
+    box.style.display = "block";
     setTimeout(() => box.style.display = "none", 1500);
 });
 
@@ -43,21 +49,23 @@ function openCrate(type) {
     startCrateAnimation();
 }
 
+function buyHealth() {
+    socket.emit("buyItem", "health");
+}
+
 function startCrateAnimation() {
     const box = document.getElementById("crateAnimation");
     box.style.display = "block";
 
-    const weapons = ["pistol", "rifle", "testi"];
+    const weapons = ["pistol", "rifle", "sniper", "laser"];
     let i = 0;
 
     const interval = setInterval(() => {
-        box.innerText = "Opening... " + weapons[i % 3];
+        box.innerText = "Opening... " + weapons[i % weapons.length];
         i++;
     }, 120);
 
-    socket.once("crateResult", () => {
-        clearInterval(interval);
-    });
+    socket.once("crateResult", () => clearInterval(interval));
 }
 
 document.addEventListener("keydown", e => {
@@ -101,6 +109,29 @@ function updateCamera() {
     camera.y = Math.max(0, Math.min(camera.y, MAP_HEIGHT - canvas.height));
 }
 
+function collectItems() {
+    const me = players[myId];
+    if (!me) return;
+
+    // Coins
+    coins.forEach((c, index) => {
+        if (me.x < c.x + 15 && me.x + 30 > c.x && me.y < c.y + 15 && me.y + 30 > c.y) {
+            socket.emit("move", {}); // dummy emit to trigger server update
+            socket.emit("collectCoin", c); // optional server-side collect if needed
+            coins.splice(index, 1);
+            me.coins += 1;
+        }
+    });
+
+    // Health packs
+    healthPacks.forEach((h, index) => {
+        if (me.x < h.x + 15 && me.x + 30 > h.x && me.y < h.y + 15 && me.y + 30 > h.y) {
+            me.hp = Math.min(100, me.hp + h.value);
+            healthPacks.splice(index, 1);
+        }
+    });
+}
+
 function drawMinimap() {
     const size = 150;
     const scaleX = size / MAP_WIDTH;
@@ -109,16 +140,24 @@ function drawMinimap() {
     ctx.fillStyle = "black";
     ctx.fillRect(20, 20, size, size);
 
+    // Players
     for (let id in players) {
         const p = players[id];
         ctx.fillStyle = id === myId ? "lime" : "red";
-        ctx.fillRect(
-            20 + p.x * scaleX,
-            20 + p.y * scaleY,
-            4,
-            4
-        );
+        ctx.fillRect(20 + p.x * scaleX, 20 + p.y * scaleY, 4, 4);
     }
+
+    // Coins
+    coins.forEach(c => {
+        ctx.fillStyle = "gold";
+        ctx.fillRect(20 + c.x * scaleX, 20 + c.y * scaleY, 3, 3);
+    });
+
+    // Health packs
+    healthPacks.forEach(h => {
+        ctx.fillStyle = "green";
+        ctx.fillRect(20 + h.x * scaleX, 20 + h.y * scaleY, 3, 3);
+    });
 }
 
 function draw() {
@@ -126,19 +165,41 @@ function draw() {
 
     handleMovement();
     updateCamera();
+    collectItems();
 
     ctx.save();
     ctx.translate(-camera.x, -camera.y);
 
+    // Map background
     ctx.fillStyle="#222";
     ctx.fillRect(0,0,MAP_WIDTH,MAP_HEIGHT);
 
+    // Coins
+    coins.forEach(c => {
+        ctx.fillStyle="gold";
+        ctx.fillRect(c.x,c.y,15,15);
+    });
+
+    // Health packs
+    healthPacks.forEach(h => {
+        ctx.fillStyle="green";
+        ctx.fillRect(h.x,h.y,20,20);
+    });
+
+    // Players
     for (let id in players) {
         const p = players[id];
         ctx.fillStyle = id===myId?"lime":"red";
         ctx.fillRect(p.x,p.y,30,30);
+
+        // HP bar
+        ctx.fillStyle = "red";
+        ctx.fillRect(p.x, p.y - 10, 30, 5);
+        ctx.fillStyle = "lime";
+        ctx.fillRect(p.x, p.y - 10, 30 * (p.hp / 100), 5);
     }
 
+    // Bullets
     bullets.forEach(b=>{
         ctx.fillStyle="yellow";
         ctx.fillRect(b.x,b.y,5,5);
@@ -147,8 +208,8 @@ function draw() {
     ctx.restore();
 
     drawMinimap();
-
     requestAnimationFrame(draw);
 }
 
 draw();
+
