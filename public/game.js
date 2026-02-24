91f4095c860ba22c8cf68614f365180d2b411e92
spidercogaming-dotcom <spider.co.gaming@gmail.com>
2026-02-24 14:10:29 +0530

    Update game.js

--- public/game.js
+++ public/game.js
@@ -1,4 +1,4 @@
-const socket = io();
+const socket = io(window.location.origin);
 
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
@@ -126,3 +126,4 @@ function draw(){
 }
 
 draw();
+
